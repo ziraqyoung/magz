7ac0cfc9359f6d6822cebf0a0cc6702e991f68f7
ziraqyoung <ziraqyoung@outlook.com>
2021-01-15 09:28:50 +0300

    Clean up schema.rb file

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 2021_01_09_085503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "tweets", force: :cascade do |t|
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "likes_count", default: 0
-    t.integer "retweets_count", default: 0
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
