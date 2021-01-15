bdcc13ee1175ea696c7fe97ff0a92eeac40ca834
ziraqyoung <ziraqyoung@outlook.com>
2021-01-15 10:02:16 +0300

    Permit users to submit name attribute during sign up and account update
    Devise allows submitting email, password &  password_confirmation
    attributes during sign up, not pretty to display user's name in the app
    
    * add name column to the users table
    * Generate RegistrationsController to include name attribute to
      devise's sign_up_params and account_update_params private methods
    * Register RegistrationsController as devise route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     get '/login', to: 'devise/sessions#new'
   end
 
-  devise_for :users 
+  devise_for :users, controllers: { registrations: 'registrations' }
 
   root to: 'pages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
