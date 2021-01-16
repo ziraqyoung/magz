97477e569bf74ddd48a90a4214d481d276e69410
ziraqyoung <ziraqyoung@outlook.com>
2021-01-16 20:44:26 +0300

    Refactor navigation bar into partials
    * set include_all_helpers = false, to configure helper to be scoped by controller instead of
    rails default behaviour(loading all helper to all views) set
    * Define collapsible_elements_partial_path helper method
    * Split the _collapsible_elements.html.erb file into partials and
    extract logic into partials

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module Magz
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.action_controller.include_all_helpers = false
   end
 end
