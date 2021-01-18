ad4c0997bd0c5af17a9542471b9e219be5527c84
ziraqyoung <ziraqyoung@outlook.com>
2021-01-18 17:03:11 +0300

    Fix typo in user's factory & add color format to rspec output

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "test#{n}"}
-    sequence(:emai) {|n| "test#{n}@example.com"}
+    sequence(:email) {|n| "test#{n}@example.com"}
     password {"123456"}
     password_confirmation {"123456"}
   end
