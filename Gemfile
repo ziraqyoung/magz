1f5c69bea04cb8d1952e2d55f1786ef8c866d119
ziraqyoung <ziraqyoung@outlook.com>
2021-01-20 10:52:40 +0300

    Add seed data
    * Add faker ruby gem
    * In seeds.rb file create methods to generate User,
      Category && Post records in the development database

--- Gemfile
+++ Gemfile
@@ -47,6 +47,7 @@ group :development do
   gem 'listen', '~> 3.3'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'faker', '~> 2.15', '>= 2.15.1'
 end
 
 group :test do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,65 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+def seed_users
+  puts "seeding users..."
+  user_id = 0
+
+  10.times do
+    User.create(
+      name: "name-#{user_id}",
+      email: "test-#{user_id}@test.com",
+      password: '123456',
+      password_confirmation: '123456'
+    )
+    user_id = user_id + 1
+  end
+end
+
+def seed_categories
+  puts "seeding categories..."
+
+  hobby = ['Arts', 'Crafts', 'Sports', 'Sciences', 'Collecting', 'Reading', 'Other']
+  study = ['Arts and Humanities', 'Physical Science and Engineering', 'Math and Logic',
+          'Computer Science', 'Data Science', 'Economics and Finance', 'Business',
+          'Social Sciences', 'Language', 'Other']
+  team = ['Study', 'Development', 'Arts and Hobby', 'Other']
+
+  hobby.each do |hobby|
+    Category.create(
+      branch: "hobby",
+      name: hobby
+    )
+  end
+
+  study.each do |study|
+    Category.create(
+      branch: "study",
+      name: study
+    )
+  end
+
+  team.each do |team|
+    Category.create(
+      branch: "team",
+      name: team
+    )
+  end
+end
+
+def seed_posts
+  puts "seeding posts..."
+  categories = Category.all
+
+  categories.each do |category|
+    5.times do
+      Post.create(
+        title: Faker::Lorem.sentences[0],
+        content: Faker::Lorem.sentences[0],
+        user_id: rand(1..9),
+        category_id: category.id
+      )
+    end
+  end
+end
+
+seed_users
+seed_categories
+seed_posts
