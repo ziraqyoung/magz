2a39f681decf03971f34c2114427cc20342c1130
ziraqyoung <ziraqyoung@outlook.com>
2021-01-26 20:34:04 +0300

    Add specs for Post model's by_category, by_branch and search scopes

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -19,5 +19,32 @@ RSpec.describe Post, type: :model do
       second_post = create(:post)
       expect(Post.all).to eq([second_post, first_post])
     end
+
+    it 'by_category scope gets posts by particular category' do
+      category = create(:category)
+      create(:post, category_id: category.id)
+      create_list(:post, 10)
+      posts = Post.by_category(category.branch, category.name)
+      expect(posts.count).to eq(1)
+      expect(posts[0].category.name).to eq(category.name)
+    end
+
+    it 'by_branch scope gets posts by particular branch' do
+      category = create(:category)
+      create(:post, category_id: category.id)
+      create_list(:post, 10)
+      posts = Post.by_branch(category.branch)
+      expect(posts.count).to eq(1)
+      expect(posts[0].category.branch).to eq(category.branch)
+    end
+
+    it 'search scope finds a matching posts' do
+      post = create(:post, title: 'awesome title', content: 'great content' * 5)
+      create_list(:post, 10, title: ('a'..'c').to_a.shuffle.join)
+      expect(Post.search('awesome').count).to eq(1)
+      expect(Post.search('awesome')[0].id).to eq(post.id)
+      expect(Post.search('great').count).to eq(1)
+      expect(Post.search('great')[0].id).to eq(post.id)
+    end
   end
 end
