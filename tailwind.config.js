452bb4369a80d35128855dc563a7367f0af0da6b
ziraqyoung <ziraqyoung@outlook.com>
2021-02-01 18:08:50 +0300

    Extend tailwind color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,32 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.blueGray,
+      indigo: colors.indigo,
+      red: colors.red,
+      yellow: colors.yellow,
+      pink: colors.pink,
+      rose: colors.rose,
+      lime: colors.lime,
+      green: colors.emerald,
+      teal: colors.teal,
+      purple: colors.purple,
+      cyan: colors.cyan,
+      blue: colors.blue,
+      orange: colors.orange,
+      amber: colors.amber,
+      violet: colors.violet,
+      fuchsia: colors.fuchsia,
+
+    },
     extend: {},
   },
   variants: {
