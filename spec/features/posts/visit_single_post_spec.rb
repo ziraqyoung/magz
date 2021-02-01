7a0dca81187dbb784b208a523efc5124a6230ae6
ziraqyoung <ziraqyoung@outlook.com>
2021-02-01 23:58:04 +0300

    Update spec

--- spec/features/posts/visit_single_post_spec.rb
+++ spec/features/posts/visit_single_post_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 RSpec.feature "Visit single post", type: :feature do
   let(:user) { create(:user) }
-  let(:post) { create(:post) }
+  let(:category) { create(:category, branch: 'hobby') }
+  let(:post) { create(:post, category: category) }
 
   scenario "User navigates to a single post from the home page" do
     post
