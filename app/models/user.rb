ec64f890f8950efd3cee64f9328ac282b5ef0d52
ziraqyoung <ziraqyoung@outlook.com>
2021-02-03 10:21:14 +0300

    Add validation of name attribute in the model and the migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true, uniqueness: true
+
   has_many :posts, dependent: :destroy
 end
