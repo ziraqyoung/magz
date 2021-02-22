0169a6471612d403fe18a96e703924a73cd25371
ziraqyoung <ziraqyoung@outlook.com>
2021-02-22 14:38:40 +0300

    Add association, helper methods and specs to User model
    * Create relationship between User and Contact models
      and add specs
    * Add methods to help query contacts records

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,40 @@ class User < ApplicationRecord
   has_many :private_messages, class_name: 'Private::Message'
   has_many :private_conversations, foreign_key: :sender_id, class_name: 'Private::Conversation'
 
+  # contacts is a join model, (allows has_many :through)
   has_many :contacts
+  has_many :all_received_contact_requests, class_name: 'Contact', foreign_key: 'contact_id'
+
+  has_many :accepted_sent_contact_requests,
+           -> { where(contacts: { accepted: true }) },
+           through: :contacts, source: :contact
+  has_many :accepted_received_contact_requests,
+           -> { where(contacts: { accepted: true }) },
+           through: :all_received_contact_requests, source: :user
+
+  has_many :pending_sent_contact_requests,
+           -> { where(contacts: { accepted: false }) },
+           through: :contacts, source: :contact
+
+  has_many :accepted_received_contact_requests,
+           -> { where(contacts: { accepted: true }) },
+           through: :all_received_contact_requests, source: :user
+  has_many :pending_received_contact_requests,
+           -> { where(contacts: { accepted: false }) },
+           through: :all_received_contact_requests, source: :user
+
+  # gets all your contacs
+  def all_active_users
+    accepted_sent_contact_requests | accepted_received_contact_requests
+  end
+
+  # gets all pending sent and receives contacts
+  def pending_contacts
+    pending_sent_contact_requests | pending_received_contact_requets
+  end
+
+  # gets a Contact record
+  def contact(contact)
+    Contact.where(user: self, contact: contact)[0]
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,5 +7,90 @@ RSpec.describe User, type: :model do
       expect(association.macro).to eq(:has_many)
       expect(association.options[:dependent]).to eq(:destroy)
     end
+
+    it 'has_many contacts' do
+      association = described_class.reflect_on_association(:contacts)
+      expect(association.macro).to eq(:has_many)
+    end
+
+    it 'has_many all_received_contact_requests' do
+      association = described_class.reflect_on_association(:all_received_contact_requests)
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:class_name]).to eq("Contact")
+      expect(association.options[:foreign_key]).to eq("contact_id")
+    end
+
+    it 'has_many accepted_sent_contact_requests' do
+      association = described_class.reflect_on_association(:accepted_sent_contact_requests)
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:through]).to eq(:contacts)
+      expect(association.options[:source]).to eq(:contact)
+    end
+
+    it 'has_many accepted_received_contact_requests' do
+      association = described_class.reflect_on_association(:accepted_received_contact_requests)
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:through]).to eq(:all_received_contact_requests)
+      expect(association.options[:source]).to eq(:user)
+    end
+
+    it 'has_many pending_sent_contact_requests' do
+      association = described_class.reflect_on_association(:pending_sent_contact_requests)
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:through]).to eq(:contacts)
+      expect(association.options[:source]).to eq(:contact)
+    end
+
+    it 'has_many pending_received_contact_requests' do
+      association = described_class.reflect_on_association(:pending_received_contact_requests)
+      expect(association.macro).to eq(:has_many)
+      expect(association.options[:through]).to eq(:all_received_contact_requests)
+      expect(association.options[:source]).to eq(:user)
+    end
+  end
+  context "Methods" do
+    let(:user) { build(:user) }
+    let(:contact_requests) do
+      @user = create(:user)
+      # dummy contacts
+      create_list(:contact, 2)
+      create_list(:contact, 2, accepted: true)
+
+      # contacts sent by user
+      create(:contact, user_id: @user.id)
+      create(:contact, user_id: @user.id, accepted: true)
+
+      # contacts received by user
+      create(:contact, contact_id: @user.id)
+      create(:contact, contact_id: @user.id, accepted: true)
+    end
+
+    before :each do
+      contact_requests
+    end
+
+    describe "#accepted_sent_contact_requests" do
+      it 'gets only accepted contact requests' do
+        expect(@user.accepted_sent_contact_requests.count).to eq(1)
+      end
+    end
+
+    describe "#accepted_received_contact_requests" do
+      it 'gets only accepted contact requests' do
+        expect(@user.accepted_received_contact_requests.count).to eq(1)
+      end
+    end
+
+    describe "#pending_sent_contact_requests" do
+      it 'gets only unaccepted contact requests' do
+        expect(@user.pending_sent_contact_requests.count).to eq(1)
+      end
+    end
+
+    describe "#pending_received_contact_requests" do
+      it 'gets only unaccepted contact requests' do
+        expect(@user.pending_received_contact_requests.count).to eq(1)
+      end
+    end
   end
 end
