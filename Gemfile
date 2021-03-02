ea8d593403115c3cbf3844dcaf415599ca4e5994
ziraqyoung <ziraqyoung@outlook.com>
2021-03-02 17:50:59 +0300

    Add hiredis gem

--- Gemfile
+++ Gemfile
@@ -14,8 +14,8 @@ gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
 gem 'webpacker', '~> 5.0'
 # Use Redis adapter to run Action Cable in production
+gem 'hiredis', '~> 0.6.3'
 gem 'redis', '~> 4.0'
-gem 'redis-rails', '~> 5.0', '>= 5.0.2'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 # Use Active Storage variant
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,8 +57,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
 
-  redis_url = ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" }
-  config.cache_store = :redis_store, redis_url
+  config.cache_store = :redis_cache_store, { url: ENV['REDIS_URL'] || 'redis://localhost:6379/1' }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
