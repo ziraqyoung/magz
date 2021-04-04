7b092f2fb0731630ee245c2449eacb96ce1b70c3
ziraqyoung <ziraqyoung@outlook.com>
2021-04-04 21:24:46 +0300

    Remove duplicate id `me_menu` in App::DropdownComponent

--- app/components/app/dropdown_component.rb
+++ app/components/app/dropdown_component.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class App::DropdownComponent < ViewComponent::Base
+  def initialize(details_id:)
+    @details_id = details_id
+  end
+
   renders_one :summary
   renders_one :body
 end
